ff1a072eb8442f506a37bed08199de26bce9f315
Pragya Singh <129204388+pragyasingh-29@users.noreply.github.com>
2023-08-15 09:37:29 +0530

    Add files via upload

---
+++ University API/app.js
@@ -0,0 +1,34 @@
+let btn = document.querySelector("button");
+let url = "http://universities.hipolabs.com/search?name=";
+
+btn.addEventListener("click", async ()=>{
+     let country = document.querySelector("input").value;
+     console.log(country);
+
+     let colArr = await getColleges(country);
+     show(colArr);
+});
+
+ function show(colArr){
+    let list = document.querySelector("#list");
+    list.innerText="";
+
+    for(col of colArr){
+        console.log(col.name);
+        let li=document.createElement("li");
+        li.innerText=col.name;
+        list.appendChild(li);
+    }
+ }
+
+
+async function getColleges(country){
+    try{
+    let link = await axios.get(url+country);
+    return link.data;
+}catch(e){
+    console.log("ERROR -", e);
+    return [];
+}
+}
+
